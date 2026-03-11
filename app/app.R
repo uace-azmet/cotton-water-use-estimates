6023782a8aa397267a8e8910be3e3593296f0fbf
Jeremy Weiss <jlweiss@email.arizona.edu>
2026-03-11 14:56:43 -0700

    wiring for input button to download data, visibility of layout elements based on data download

--- app/app.R
+++ app/app.R
@@ -22,7 +22,8 @@ ui <- htmltools::htmlTemplate(
       sidebar = pageSidebar, # `scr##_pageSidebar.R`
       shiny::htmlOutput(outputId = "navsetCardTabTitle"),
       shiny::htmlOutput(outputId = "navsetCardTabSummary"),
-      navsetCardTab # `scr##_navsetCardTab.R`
+      # navsetCardTab # `scr##_navsetCardTab.R`
+      shiny::uiOutput(outputId = "navsetCardTab")
     ) |>
       htmltools::tagAppendAttributes(
         #https://getbootstrap.com/docs/5.0/utilities/api/
@@ -38,9 +39,19 @@ ui <- htmltools::htmlTemplate(
 
 
 server <- function(input, output, session) {
+  # shinyjs::useShinyjs(html = TRUE)
+  # shinyjs::hideElement("pageBottomText")
+  
   
   # Observables -----
   
+  shiny::observeEvent(azDaily(), {
+    showNavsetCardTab(TRUE)
+    showPageBottomText(TRUE)
+    # shinyjs::showElement("pageBottomText")
+  })
+  
+  # To update available dates based on selected station
   shiny::observeEvent(input$azmetStation, {
     stationStartDate <-
       dplyr::filter(
@@ -87,23 +98,53 @@ server <- function(input, output, session) {
     )
   })
   
+  # To update icon in navsetCardTab title
   shiny::observeEvent(input$navsetCardTab, {
     if (input$navsetCardTab == "barChart") {
       navsetCardTabTitleIcon("bar-chart-fill")
-      print("bar-chart-fill")
+      # print("bar-chart-fill")
     } else if (input$navsetCardTab == "table") {
       navsetCardTabTitleIcon("table")
-      print("table")
+      # print("table")
     } else if (input$navsetCardTab == "timeSeries") {
       navsetCardTabTitleIcon("graph-up")
-      print("graph-up")
+      # print("graph-up")
     }
   })
   
   
-  # Outputs -----
+  # Reactives -----
   
-  output$navsetCardTabSummary <- shiny::renderUI({
+  azDaily <- shiny::eventReactive(input$estimateWaterUse, {
+    shiny::validate(
+      shiny::need(
+        expr = input$startDate <= input$endDate,
+        message = FALSE
+      )
+    )
+    
+    idEstimateWaterUse <- shiny::showNotification(
+      ui = "Estimating water use . . .",
+      action = NULL,
+      duration = NULL,
+      closeButton = FALSE,
+      id = "idEstimateWaterUse",
+      type = "message"
+    )
+    
+    on.exit(
+      shiny::removeNotification(id = idEstimateWaterUse),
+      add = TRUE
+    )
+    
+    fxn_azDaily(
+      azmetStation = input$azmetStation,
+      startDate = input$startDate,
+      endDate = input$endDate
+    )
+  })
+  
+  navsetCardTabSummary <- shiny::eventReactive(azDaily(), {
     fxn_navsetCardTabSummary(
       azmetStation = input$azmetStation,
       startDate = input$startDate,
@@ -111,16 +152,31 @@ server <- function(input, output, session) {
     )
   })
   
-  output$navsetCardTabTitle <- shiny::renderUI({
+  navsetCardTabTitle <- shiny::eventReactive(azDaily(), {
     fxn_navsetCardTabTitle(
-      azmetStation = input$azmetStation, 
+      azmetStation = input$azmetStation,
       navsetCardTabTitleIcon = navsetCardTabTitleIcon()
     )
-    # figureTitle()
+  })
+  
+  
+  # Outputs -----
+  
+  output$navsetCardTab <- shiny::renderUI({
+    shiny::req(showNavsetCardTab())
+    navsetCardTab # `scr##_navsetCardTab.R`
+  })
+  
+  output$navsetCardTabSummary <- shiny::renderUI({
+    navsetCardTabSummary()
+  })
+  
+  output$navsetCardTabTitle <- shiny::renderUI({
+    navsetCardTabTitle()
   })
   
   output$pageBottomText <- shiny::renderUI({
-    #shiny::req()
+    shiny::req(showPageBottomText())
     fxn_pageBottomText()
   })
 }
