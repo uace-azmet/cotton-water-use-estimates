40f307150f2fe11190e72927bf2cc5b3276c8d7d
Jeremy Weiss <jlweiss@email.arizona.edu>
2026-03-30 14:57:24 -0700

    Correct YUG nodata dates in error modal

--- app/R/scr05_datepickerYumaNGilaErrorModal.R
+++ app/R/scr05_datepickerYumaNGilaErrorModal.R
@@ -1,7 +1,7 @@
 datepickerYumaNGilaErrorModal <- 
   shiny::modalDialog(
     htmltools::HTML(
-      "<em>The Yuma N.Gila station was not in operation from June 16, 2021 through October 10, 2021. To specify a period of interest during that year, please select both a <b>Planting Date</b> and <b>End Date</b> prior to or after this period."
+      "<em>The Yuma N.Gila station was not in operation from June 16, 2021 through October 21, 2021. To specify a period of interest during that year, please select both a <b>Planting Date</b> and <b>End Date</b> prior to or after this period."
     ),
     easyClose = FALSE,
     fade = FALSE,
